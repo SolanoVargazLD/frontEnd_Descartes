4a31ac4b07b4d830adf0ea0c9b905b2456bfbefd
SolanoVargazLD <mludan95@gmail.com>
2023-05-19 17:06:43 -0600

    view_Aspirant_Maestria_Doctorado

--- src/app/components/dashboard/nivel-educative/nivel-superior/posgrado/show-posgrado/select-maestria-doctorado/select-maestria-doctorado.component.ts
+++ src/app/components/dashboard/nivel-educative/nivel-superior/posgrado/show-posgrado/select-maestria-doctorado/select-maestria-doctorado.component.ts
@@ -1,53 +1,53 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import { listSuperior } from 'src/app/interface/list_levelHigher_interface';
-import { ServiceDataPosgradoService } from 'src/app/service/aspirantSuperior/posgrado/data-posgrado/service-data-posgrado.service';
-import { ShowPosgradoComponent } from '../show-posgrado.component';
-import { ServicioIdPosgradoService } from 'src/app/service/share_Inf/superior/posgrado/servicio-id-posgrado.service';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnChanges,
+    OnInit,
+    Output,
+    SimpleChanges
+} from '@angular/core';
+import {listSuperior} from 'src/app/interface/list_levelHigher_interface';
+import {ServiceDataPosgradoService} from 'src/app/service/aspirantSuperior/posgrado/data-posgrado/service-data-posgrado.service';
+import {ShowPosgradoComponent} from '../show-posgrado.component';
+import {ServicioIdPosgradoService} from 'src/app/service/share_Inf/superior/posgrado/servicio-id-posgrado.service';
 
-@Component({
-  selector: 'app-select-maestria-doctorado',
-  templateUrl: './select-maestria-doctorado.component.html',
-  styleUrls: ['./select-maestria-doctorado.component.css']
-})
-export class SelectMaestriaDoctoradoComponent  implements OnInit, OnChanges{
-  @Input() selectedNivel: string= 'Maestria';
+@Component({selector: 'app-select-maestria-doctorado', templateUrl: './select-maestria-doctorado.component.html', styleUrls: ['./select-maestria-doctorado.component.css']})
+export class SelectMaestriaDoctoradoComponent implements OnInit,
+OnChanges {
+    @Input()selectedNivel : string = 'Maestria';
 
-  @Input() selectedPosgrado: string;
-  dataPos: listSuperior[]=[];
-  @Output() selectEventPos= new EventEmitter<string>();
-  @Output() selectEventNivelPos= new EventEmitter<string>();
+    @Input()selectedPosgrado : string;
+    dataPos : listSuperior[] = [];
+    @Output()selectEventPos = new EventEmitter<string>();
+    @Output()selectEventNivelPos = new EventEmitter<string>();
 
-  constructor(private serviceDataPosgradoService: ServiceDataPosgradoService,
-    private servicioIdPosgradoService: ServicioIdPosgradoService,
-    private compPadre: ShowPosgradoComponent,
-    ){}
+    constructor(private serviceDataPosgradoService : ServiceDataPosgradoService, private servicioIdPosgradoService : ServicioIdPosgradoService, private compPadre : ShowPosgradoComponent,) {}
 
-  ngOnInit(): void {
-    // this.loadData();
-  }
+    ngOnInit(): void {}
 
-  ngOnChanges(changes: SimpleChanges) {
-    if ('selectedNivel' in changes) {
-      this.loadData();
-    }
-  }
-
-  enviarDataSelect(){
-    this.selectEventPos.emit(this.selectedNivel);
-    this.selectEventNivelPos.emit(this.selectedPosgrado);
-    this.compPadre.recargarData();
-  }
-
-  loadData(){
-    this.serviceDataPosgradoService.getListPosgradoCareer(this.selectedNivel).subscribe(data=> {
-          this.dataPos = data;
-          if(this.servicioIdPosgradoService.getSelectedPosgrad()==''){
-            this.selectedPosgrado=data[0].name;
-          }else{
-            this.selectedPosgrado= this.servicioIdPosgradoService.getSelectedPosgrad()!=data[0].name?this.servicioIdPosgradoService.getSelectedPosgrad():data[0].name ;
-          }
-          this.enviarDataSelect();
+    ngOnChanges(changes : SimpleChanges) {
+        if ('selectedNivel' in changes) {
+            this.loadData();
         }
-    );
-  }
+    }
+
+    enviarDataSelect() {
+        this.selectEventPos.emit(this.selectedNivel);
+        this.selectEventNivelPos.emit(this.selectedPosgrado);
+        this.compPadre.recargarData();
+    }
+
+    loadData() {
+        this.serviceDataPosgradoService.getListPosgradoCareer(this.selectedNivel).subscribe(data => {
+            this.dataPos = data;
+            if (this.servicioIdPosgradoService.getSelectedPosgrad() == '') {
+                this.selectedPosgrado = data[0].name;
+            } else {
+                this.selectedPosgrado = this.servicioIdPosgradoService.getSelectedPosgrad() != data[0].name ? this.servicioIdPosgradoService.getSelectedPosgrad() : data[0].name;
+                this.servicioIdPosgradoService.setSelectedPosgrad('');
+            }
+            this.enviarDataSelect();
+        });
+    }
 }
--- src/app/components/dashboard/nivel-educative/nivel-superior/posgrado/show-posgrado/show-posgrado.component.ts
+++ src/app/components/dashboard/nivel-educative/nivel-superior/posgrado/show-posgrado/show-posgrado.component.ts
@@ -1,126 +1,114 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-import { ServiceAspirantService } from 'src/app/service/aspirant/service-aspirant.service';
-import { ServiceDataPosgradoService } from 'src/app/service/aspirantSuperior/posgrado/data-posgrado/service-data-posgrado.service';
-import { AspirantPostgrado } from 'src/app/interface/aspirant_posgrado';
-import { ServicioAspirantPosgradoService } from 'src/app/service/aspirantSuperior/posgrado/view-posgrado/servicio-aspirant-posgrado.service';
-import { ServicioIdPosgradoService } from 'src/app/service/share_Inf/superior/posgrado/servicio-id-posgrado.service';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import {ServiceAspirantService} from 'src/app/service/aspirant/service-aspirant.service';
+import {ServiceDataPosgradoService} from 'src/app/service/aspirantSuperior/posgrado/data-posgrado/service-data-posgrado.service';
+import {AspirantPostgrado} from 'src/app/interface/aspirant_posgrado';
+import {ServicioAspirantPosgradoService} from 'src/app/service/aspirantSuperior/posgrado/view-posgrado/servicio-aspirant-posgrado.service';
+import {ServicioIdPosgradoService} from 'src/app/service/share_Inf/superior/posgrado/servicio-id-posgrado.service';
 
 import Swal from 'sweetalert2';
 
-@Component({
-  selector: 'app-show-posgrado',
-  templateUrl: './show-posgrado.component.html',
-  styleUrls: ['./show-posgrado.component.css']
-})
+@Component({selector: 'app-show-posgrado', templateUrl: './show-posgrado.component.html', styleUrls: ['./show-posgrado.component.css']})
 export class ShowPosgradoComponent implements OnInit {
-  selected: string = 'Maestria';
-  selectedPosgrado: string ;
-  displayedColumns: string[] = ['Id', 'name', 'lastNameP', 'lastNameM', 'cedula', 'operations'];
-  dataSource: any;
-  dataVacio: boolean = false;
-  dataTamanio: number = 0;
-  tipeBusqueda: string = '';
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
+    selected : string = 'Maestria';
+    selectedPosgrado : string;
+    displayedColumns : string[] = [
+        'Id',
+        'name',
+        'lastNameP',
+        'lastNameM',
+        'cedula',
+        'operations'
+    ];
+    dataSource : any;
+    dataVacio : boolean = false;
+    dataTamanio : number = 0;
+    tipeBusqueda : string = '';
+    @ViewChild(MatPaginator)paginator !: MatPaginator;
 
-  constructor(private serviceDataPosgradoService: ServiceDataPosgradoService,
-    private servicioAspirantPosgradoService: ServicioAspirantPosgradoService,
-    private servicioIdPosgradoService: ServicioIdPosgradoService,
-    private serviceAspirantService: ServiceAspirantService) { }
+    constructor(private serviceDataPosgradoService : ServiceDataPosgradoService, private servicioAspirantPosgradoService : ServicioAspirantPosgradoService, private servicioIdPosgradoService : ServicioIdPosgradoService, private serviceAspirantService : ServiceAspirantService) {}
 
-  ngOnInit(): void {
-    if (this.servicioIdPosgradoService.getSelectedNivel() == '') {
-      this.readDataServiceAspirantBasic();
-    } else {
-      this.selected = this.servicioIdPosgradoService.getSelectedNivel();
-      this.selectedPosgrado= this.servicioIdPosgradoService.getSelectedPosgrad();
-      this.recargarData();
+    ngOnInit(): void {
+        if (this.servicioIdPosgradoService.getSelectedNivel() == '') {
+            this.readDataServiceAspirantBasic();
+        }
     }
-  }
 
-  recargarData() {
-    console.log("Nivel "+ this.selected +" Posgrado "+this.selectedPosgrado);
-      this.servicioAspirantPosgradoService.getlistPosgrado(this.selectedPosgrado, this.selected).subscribe({
-          next: data => {
-            this.dataSource = new MatTableDataSource<AspirantPostgrado>(data);
-            this.dataSource.paginator = this.paginator;
-            this.dataVacio = (data.length == 0) ? true : false;
-            this.dataTamanio = data.length;
-          },
-          error: error => {
-            console.log("Error: " + error);
-          }
+    recargarData() {
+        this.servicioAspirantPosgradoService.getlistPosgrado(this.selectedPosgrado, this.selected).subscribe({
+            next: data => {
+                this.dataSource = new MatTableDataSource<AspirantPostgrado>(data);
+                this.dataSource.paginator = this.paginator;
+                this.dataVacio = (data.length == 0) ? true : false;
+                this.dataTamanio = data.length;
+            },
+            error: error => {
+                console.log("Error: " + error);
+            }
         });
-  }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
+        if (this.servicioIdPosgradoService.getSelectedNivel() != '') {
+            this.servicioIdPosgradoService.setIdAspirant(0);
+            this.servicioIdPosgradoService.setIdAspirantPosgrado(0);
+            this.servicioIdPosgradoService.setSelectedNivel('');
+            this.servicioIdPosgradoService.setSelectedPosgrad('');
+        }
 
-  readDataServiceAspirantBasic() {
-            console.log();
-    this.serviceDataPosgradoService.getListPosgradoCareer(this.selected).subscribe(data => {
-      console.log("selectedRDSAB: "+ this.selected + ": postRDSAB: "+ data[0].name);
-      this.servicioAspirantPosgradoService.getlistPosgrado(data[0].name, this.selected).subscribe({
-          next: data => {
-            this.dataSource = new MatTableDataSource<AspirantPostgrado>(data);
-            this.dataSource.paginator = this.paginator;
-            this.dataVacio = (data.length == 0) ? true : false;
-            this.dataTamanio = data.length;
-          },
-          error: error => {
-            console.log("Error: " + error);
-          }
+
+    }
+
+    applyFilter(event : Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
+
+    readDataServiceAspirantBasic() {
+        this.serviceDataPosgradoService.getListPosgradoCareer(this.selected).subscribe(data => {
+            this.servicioAspirantPosgradoService.getlistPosgrado(data[0].name, this.selected).subscribe({
+                next: data => {
+                    this.dataSource = new MatTableDataSource<AspirantPostgrado>(data);
+                    this.dataSource.paginator = this.paginator;
+                    this.dataVacio = (data.length == 0) ? true : false;
+                    this.dataTamanio = data.length;
+                },
+                error: error => {
+                    console.log("Error: " + error);
+                }
+            });
         });
-    });
-  }
+    }
 
-  openDialog(id_asp: number, nombre: string, apP: string, apM: string, curp: string): void {
-    Swal.fire({
-      title: 'ADVERTENCIA',
-      text: `Esta seguro(a) de eliminar al Aspirante de Bachillerato ${nombre} ${apP} ${apM} - ${curp}`,
-      icon: 'error',
-      showCancelButton: true,
-      confirmButtonColor: '#2a53c4',
-      cancelButtonColor: '#ff3a3a',
-      confirmButtonText: 'Aceptar'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        console.log(id_asp);
+    openDialog(id_asp : number, nombre : string, apP : string, apM : string, curp : string): void {
+        Swal.fire({
+            title: 'ADVERTENCIA',
+            text: `Esta seguro(a) de eliminar al Aspirante de Bachillerato ${nombre} ${apP} ${apM} - ${curp}`,
+            icon: 'error',
+            showCancelButton: true,
+            confirmButtonColor: '#2a53c4',
+            cancelButtonColor: '#ff3a3a',
+            confirmButtonText: 'Aceptar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                console.log(id_asp);
 
-        let resp = this.serviceAspirantService.deleteAspirant(id_asp).subscribe();
-        if (resp) {
-          Swal.fire(
-            'Eliminado',
-            'Aspirante eliminado de la lista',
-            'success'
-          ).then((result) => {
-            // this.recargarData();
-          })
-        } else {
-          Swal.fire(
-            'Fallo',
-            'Aspirante no eliminado, revisar informacion.',
-            'question'
-          );
-        };
-      }
-    });
+                let resp = this.serviceAspirantService.deleteAspirant(id_asp).subscribe();
+                if (resp) {
+                    Swal.fire('Eliminado', 'Aspirante eliminado de la lista', 'success').then((result) => { // this.recargarData();
+                    })
+                } else {
+                    Swal.fire('Fallo', 'Aspirante no eliminado, revisar informacion.', 'question');
+                };
+            }
+        });
 
-  }
+    }
 
-  clickViewAspirant(id: number, id_b: number) {
-    this.servicioIdPosgradoService.setIdAspirant(id);
-    this.servicioIdPosgradoService.setIdAspirantPosgrado(id_b);
-    this.servicioIdPosgradoService.setSelectedNivel(this.selected);
-    this.servicioIdPosgradoService.setSelectedPosgrad(this.selectedPosgrado);
-    console.log("id_aspirant: "+ id);
-    console.log("id_aspirantPosgrado: "+ id_b);
-    console.log("id_selectedN: "+ this.selected);
-    console.log("id_selectedP: "+ this.selectedPosgrado);
-
-  }
+    clickViewAspirant(id : number, id_b : number) {
+        this.servicioIdPosgradoService.setIdAspirant(id);
+        this.servicioIdPosgradoService.setIdAspirantPosgrado(id_b);
+        this.servicioIdPosgradoService.setSelectedNivel(this.selected);
+        this.servicioIdPosgradoService.setSelectedPosgrad(this.selectedPosgrado);
+    }
 
 }
